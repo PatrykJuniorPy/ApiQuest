7fa4ad47d9d60ecd3ad775cb6c8cd312b71367b4
unknown <guru090192@gmail.com>
2022-06-28 20:40:53 +0200

    added gitignore and modified views

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books),
+    path('books/', views.books_list),
+    path('', views.books),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('books/<int:pk>/', views.books_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -3,9 +3,61 @@ from django.shortcuts import render
 import requests
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from api.models import Books
+from api.serializers import BooksSerializer
 
 
 @api_view(['GET'])
 def books(request):
     r = requests.get("https://www.googleapis.com/books/v1/volumes?q=Hobbit")
     return Response(r.json())
+
+
+@csrf_exempt
+def books_list(request):
+    """
+    List all code books, or create a new book
+
+    """
+    if request.method == 'GET':
+        books = Books.objects.all()
+        serializer = BooksSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BooksSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+# @csrf_exempt
+# def books_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code book.
+#     """
+#     try:
+#         books = Books.objects.get(pk=pk)
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)
+
+#     if request.method == 'GET':
+#         serializer = BooksSerializer(books)
+#         return JsonResponse(serializer.data)
+
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = BooksSerializer(books, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == 'DELETE':
+#         books.delete()
+#         return HttpResponse(status=204)
